b6a70039519669e030a58a5063f65a9d7b049a7e
Ates Goral <ates.goral@shopify.com>
2018-04-02 00:03:37 -0400

    Use own stream parser

--- index.js
+++ index.js
@@ -1,203 +1,124 @@
-const Transform = require('stream').Transform;
-const Parser = require('stream-parser');
+const StreamParser = require('./stream-parser');
 
-const colorSpaceMap = {
-  0: 'RGB',
-  2: 'CMYK',
-  7: 'Lab'
-};
-
-const componentNormalizerMap = {
-  RGB: (components) => components,
-  CMYK: (components) => ({
-    c: Math.round((255 - components[0]) / 2.55),
-    m: Math.round((255 - components[1]) / 2.55),
-    y: Math.round((255 - components[2]) / 2.55),
-    k: Math.round((255 - components[3]) / 2.55)
-  }),
-  Lab: (components) => ({
-    l: Math.round(components[0] / 2.55),
-    a: components[1] - 128,
-    b: components[2] - 128
-  })
-};
-
-class AcbParser extends Transform {
+class AcbParser extends StreamParser {
   constructor(options) {
     super(options);
 
-    this.colorBook = {};
-
-    this._bytes(4, this.onSignature);
+    this.readBook()
+      .then((book) => this.emit('book', book))
+      .catch((error) => this.emit('error', error));
   }
 
-  readInt8(cb) {
-    this._bytes(1, (buffer, output) => {
-      cb.call(this, buffer.readUInt8(0), output);
-    });
+  async readString() {
+    const length = await this.readUInt32BE();
+
+    if (length) {
+      return (await this.read(length * 2)).swap16().toString('utf16le');
+    } else {
+      return '';
+    }
   }
 
-  readInt16(cb) {
-    this._bytes(2, (buffer, output) => {
-      cb.call(this, buffer.readUInt16BE(0), output);
-    });
+  async readColorSpace() {
+    const colorSpace = {
+      0: 'RGB',
+      2: 'CMYK',
+      7: 'Lab'
+    }[await this.readUInt16BE()]
+
+    if (!colorSpace) {
+      throw new Error('Unknown color space');
+    }
   }
 
-  readInt32(cb) {
-    this._bytes(4, (buffer, output) => {
-      cb.call(this, buffer.readUInt32BE(0), output);
-    });
+  async readComponents(colorSpace) {
+    switch (colorSpace) {
+    case 'RGB':
+      return {
+        r: await this.readUInt8(),
+        g: await this.readUInt8(),
+        b: await this.readUInt8()
+      };
+    case 'CMYK':
+      return {
+        c: Math.round((255 - await this.readUInt8()) / 2.55),
+        m: Math.round((255 - await this.readUInt8()) / 2.55),
+        y: Math.round((255 - await this.readUInt8()) / 2.55),
+        k: Math.round((255 - await this.readUInt8()) / 2.55)
+      };
+    case 'Lab':
+      return {
+        l: Math.round(await this.readUInt8() / 2.55),
+        a: await this.readUInt8() - 128,
+        b: await this.readUInt8() - 128
+      };
+    }
   }
 
-  readString(cb) {
-    this.readInt32((length, output) => {
-      if (length) {
-        this._bytes(length * 2, (buffer, output) => {
-          cb.call(this, buffer.swap16().toString('utf16le'), output);
-        });
-      } else {
-        cb('', output);
-      }
-    });
+  async readColor(colorSpace) {
+    const color = {};
+
+    color.name = await this.readString();
+    color.code = (await this.read(6)).toString('ascii');
+    color.components = await this.readComponents(colorSpace);
+
+    return color;
   }
 
-  readColors(cb) {
+  async readColors(colorCount, colorSpace) {
     const colors = [];
-    const componentCount = this.colorBook.colorSpace === 'CMYK' ? 4 : 3;
 
-    const readNextColor = () => {
-      if (colors.length === this.colorBook.colorCount) {
-        cb.call(this, colors);
-      } else {
-        const color = {};
-
-        this.readString((name) => {
-          color.name = name;
-
-          this._bytes(6, (buffer) => {
-            color.code = buffer.toString('ascii');
-
-            const components = [];
-
-            const readNextComponent = () => {
-              if (components.length === componentCount) {
-                color.components = componentNormalizerMap[this.colorBook.colorSpace](components);
-                colors.push(color);
-                readNextColor();
-              } else {
-                this.readInt8((component) => {
-                  components.push(component);
-                  readNextComponent();
-                });
-              }
-            };
-
-            readNextComponent();
-          })
-        });
-      }
-    };
-
-    readNextColor();
-  }
-
-  onSignature(buffer, output) {
-    this.colorBook.signature = buffer.toString('ascii');
-
-    if (this.colorBook.signature === '8BCB') {
-      this.readInt16(this.onVersion);
-    } else {
-      output(new Error('Not an ACB file'));
+    while (colors.length < colorCount) {
+      colors.push(await this.readColor(colorSpace));
     }
+
+    return colors;
   }
 
-  onVersion(version, output) {
-    this.colorBook.version = version;
+  async readIsSpot() {
+    return (await this.read(8)).toString('ascii') === 'spflspot';
+  }
 
-    if (this.colorBook.version === 1) {
-      this.readInt16(this.onId);
-    } else {
-      output(new Error('Invalid version'));
+  async readBook() {
+    const book = {};
+
+    book.signature = (await this.read(4)).toString('ascii');
+
+    if (book.signature !== '8BCB') {
+      throw new Error('Not an ACB file');
     }
-  }
 
-  onId(id, output) {
-    this.colorBook.id = id;
-    this.readString(this.onTitle);
-  }
+    book.version = await this.readUInt16BE();
 
-  onTitle(title, output) {
-    this.colorBook.title = title;
-    this.readString(this.onColorNamePrefix);
-  }
+    if (book.version !== 1) {
+      throw new Error('Invalid version');
+    }
 
-  onColorNamePrefix(colorNamePrefix, output) {
-    this.colorBook.colorNamePrefix = colorNamePrefix;
-    this.readString(this.onColorNameSuffix);
-  }
+    // book.id = await this.readUInt16BE();
+    // book.title = await this.readString();
+    // book.colorNamePrefix = await this.readString();
+    // book.colorNameSuffix = await this.readString();
+    // book.description = await this.readString();
+    // book.colorCount = await this.readUInt16BE();
+    // book.pageSize = await this.readUInt16BE();
+    // book.pageMidPoint = await this.readUInt16BE();
+    // book.colorSpace = await this.readColorSpace();
+    // book.colors = await this.readColors();
+    // book.isSpot = await this.readIsSpot();
 
-  onColorNameSuffix(colorNameSuffix, output) {
-    this.colorBook.colorNameSuffix = colorNameSuffix;
-    this.readString(this.onDescription);
-  }
-
-  onDescription(description, output) {
-    this.colorBook.description = description;
-    this.readInt16(this.onColorCount);
-  }
-
-  onColorCount(colorCount, output) {
-    this.colorBook.colorCount = colorCount;
-    this.readInt16(this.onPageSize);
-  }
-
-  onPageSize(pageSize, output) {
-    this.colorBook.pageSize = pageSize;
-    this.readInt16(this.onPageMidpoint);
-  }
-
-  onPageMidpoint(pageMidPoint, output) {
-    this.colorBook.pageMidPoint = pageMidPoint;
-    this.readInt16(this.onColorSpace);
-  }
-
-  onColorSpace(colorSpace, output) {
-    this.colorBook.colorSpace = colorSpaceMap[colorSpace];
-    this.readColors(this.onColors);
-  }
-
-  onColors(colors) {
-    this.colorBook.colors = colors;
-    this._bytes(8, this.onProduction);
-  }
-
-  onProduction(production) {
-    this.colorBook.isSpot = production.toString('ascii') === 'spflspot';
-    this.emit('done', this.colorBook);
-    //this._passthrough(Infinity);
+    return book;
   }
 }
 
-Parser(AcbParser.prototype);
-
 const parser = new AcbParser();
 
-parser.on('done', function (book) {
-  console.log('done');
-  //console.log('Got book:', JSON.stringify(book, null, 2));
+parser.on('book', (book) => {
+  console.log('Got book', book);
 });
 
-parser.on('error', function (error) {
-  console.error('Got error:', error.message);
+parser.on('error', (error) => {
+  console.error('Got error', error);
 });
 
-process.stdin.pipe(parser);//.pipe(process.stdout);
+process.stdin.pipe(parser);
 process.stdin.resume();
-
-// exports.encode = (colorBook) => {
-
-// };
-
-// exports.decode = (buffer) => {
-
-// };
---
+++ stream-parser.js
@@ -0,0 +1,106 @@
+const Writable = require('stream').Writable;
+
+class StreamParser extends Writable {
+  constructor(options) {
+    super(options);
+    this.buffer = Buffer.from([]); // @todo use allocUnsafe?
+    this.readQueue = [];
+    this.readQueueResolver = null;
+  }
+
+  queueRead(count, resolve) {
+    console.log('queueRead', count);
+
+    this.readQueue.push({ count, resolve });
+
+    if (this.readQueueResolver) {
+      console.log('resolving read queue');
+      this.readQueueResolver(); // @todo need to reset?
+    }
+  }
+
+  readQueued() {
+    console.log('readQueued');
+
+    return new Promise((resolve, reject) => {
+      if (this.readQueue.length) {
+        console.log('resolving');
+        resolve(this.readQueue);
+      } else {
+        console.log('setting resolver');
+        this.readQueueResolver = resolve;
+      }
+    });
+  }
+
+  async depleteChunk(chunk) {
+    console.log('depleteChunk');
+
+    // let c = 0;
+
+    while (chunk.length) {
+      // c++;
+
+      console.log('awaiting queue');
+      // const queue = await this.readQueued();
+      await this.readQueued().then((queue) => {
+        console.log('got queued reads', queue.length);
+
+        const read = queue.shift(); // @todo loop over queue instead of awaiting again
+
+        const haveEnoughBytesInChunk = read.count <= chunk.length;
+
+        if (haveEnoughBytesInChunk) {
+          read.resolve(chunk.slice(0, read.count));
+          chunk = chunk.slice(read.count); // @todo increment a start index instead of slicing
+        } else {
+          this.buffer = Buffer.concat(this.buffer, chunk);
+          return Promise.resolve();
+        }
+      });
+    }
+  }
+
+  _write(chunk, encoding, callback) {
+    console.log('_write', chunk.length);
+
+    this.depleteChunk(chunk)
+      .then(callback)
+      .catch(callback);
+  }
+
+  // @todo implement _writev?
+
+  // @todo implement _destroy
+
+  // @todo implement entire Buffer interface?
+  read(count) {
+    return new Promise((resolve, reject) => {
+      this.queueRead(count, resolve); // @todo return Promise from that instead?
+    });
+  }
+
+  async readUInt8() {
+    return (await this.read(1)).readUInt8(0);
+  }
+
+  async readUInt16BE() {
+    return (await this.read(2)).readUInt16BE(0);
+  }
+
+  async readUInt32BE() {
+    return (await this.read(2)).readUInt32BE(0);
+  }
+
+  // readUInt16BE() {
+  //   return this.read(2).then((buffer) => buffer.readUInt16BE(0));
+  // }
+
+  // @todo to signal no need to deplete latest chunk
+  // pass thru?
+  end() {
+
+  }
+}
+
+module.exports = StreamParser;
