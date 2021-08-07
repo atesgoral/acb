b0c584d20f03ef90136c901691471939aec667bb
Ates Goral <ates@magnetiq.com>
2021-08-07 12:51:08 -0400

    Prettify

--- src/lib/tests/chunk.test.ts
+++ src/lib/tests/chunk.test.ts
@@ -1,4 +1,4 @@
-import * as Chunk  from '../chunk';
+import * as Chunk from '../chunk';
 
 describe('fromAscii', () => {
   test('returns a buffer from the given ASCII string', () => {
@@ -8,19 +8,23 @@ describe('fromAscii', () => {
 
 describe('fromUInt16BE', () => {
   test('returns a buffer from the given big-endian unsigned 16-bit number', () => {
-    expect(Chunk.fromUInt16BE(0xDEAD)).toEqual(Buffer.from([0xDE, 0xAD]));
+    expect(Chunk.fromUInt16BE(0xdead)).toEqual(Buffer.from([0xde, 0xad]));
   });
 });
 
 describe('fromUInt32BE', () => {
   test('returns a buffer from the given big-endian unsigned 32-bit number', () => {
-    expect(Chunk.fromUInt32BE(0xDEADBEEF)).toEqual(Buffer.from([0xDE, 0xAD, 0xBE, 0xEF]));
+    expect(Chunk.fromUInt32BE(0xdeadbeef)).toEqual(
+      Buffer.from([0xde, 0xad, 0xbe, 0xef])
+    );
   });
 });
 
 describe('fromString', () => {
   test('returns a buffer from the given string', () => {
-    expect(Chunk.fromString('hello')).toEqual(Buffer.from('\x00\x00\x00\x05\x00h\x00e\x00l\x00l\x00o'));
+    expect(Chunk.fromString('hello')).toEqual(
+      Buffer.from('\x00\x00\x00\x05\x00h\x00e\x00l\x00l\x00o')
+    );
     expect(Chunk.fromString('')).toEqual(Buffer.from('\x00\x00\x00\x00'));
   });
 });
